b005ac887275623339a9c69a7f69b35ab59c7e9b
Andrew Berry <andrew@furrypaws.ca>
2023-11-02 14:38:49 -0400

    Add missing ddev-generated tags

--- xhgui-mongo/mongo.init.d
+++ xhgui-mongo/mongo.init.d
@@ -1,3 +1,4 @@
+#ddev-generated
 db.results.ensureIndex( { 'meta.SERVER.REQUEST_TIME' : -1 } );
 db.results.ensureIndex( { 'profile.main().wt' : -1 } );
 db.results.ensureIndex( { 'profile.main().mu' : -1 } );
