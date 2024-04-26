1a961ceee36e9d616ecbe9df5d13ce7de3de401d
tyler36 <7234392+tyler36@users.noreply.github.com>
2024-04-26 10:46:46 +0900

    Use default DDEV database

--- xhgui/xhgui.config.php
+++ xhgui/xhgui.config.php
@@ -14,9 +14,9 @@ return [
 
     // Database options for PDO.
     'pdo' => [
-        'dsn' => getenv('XHGUI_PDO_DSN') ?: null,
-        'user' => getenv('XHGUI_PDO_USER') ?: null,
-        'pass' => getenv('XHGUI_PDO_PASS') ?: null,
+        'dsn' => getenv('XHGUI_PDO_DSN') ?: 'mysql:host=db;dbname=db',
+        'user' => getenv('XHGUI_PDO_USER') ?: 'db',
+        'pass' => getenv('XHGUI_PDO_PASS') ?: 'db',
         'table' => getenv('XHGUI_PDO_TABLE') ?: 'results',
         'tableWatch' => getenv('XHGUI_PDO_TABLE_WATCHES') ?: 'watches',
     ],
