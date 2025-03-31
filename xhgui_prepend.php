c4fa562b43a41145e37db23928d8bc332aba2872
Stanislav Zhuk <stasadev@gmail.com>
2025-03-31 14:04:44 +0300

    fix: use correct autoloader and bump to 0.23.2

--- xhgui_prepend.php
+++ xhgui_prepend.php
@@ -3,7 +3,7 @@
 // xhgui_prepend.php is copied to xhprof_prepend.php to set up xhgui
 // Overrides DDEV's built in xhprof handler.
 $homeDir = getenv('HOME');
-$globalAutoload = $homeDir . '/.composer/vendor/autoload.php';
+$globalAutoload = $homeDir . '/.composer/vendor/perftools/php-profiler/autoload.php';
 if (file_exists($globalAutoload)) {
     require_once $globalAutoload;
     // echo "Global autoloader loaded successfully from: $globalAutoload\n";
